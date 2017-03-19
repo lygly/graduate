0375d95e3fc6586b8ae4f24574890c3e4698a6e8
lygly <1516045937@qq.com>
2017-03-19 23:49:17 +0800

    日期插件设置中

--- resources/views/admin/customer/add.blade.php
+++ resources/views/admin/customer/add.blade.php
@@ -93,7 +93,8 @@
                     <tr>
                         <th>出生日期：</th>
                         <td>
-                            <input  type="text" id="birthday" name="birthday" style="width:170px;padding:7px 10px;border:1px solid #ccc;margin-right:10px;" placeholder="出生日期">
+                            <div>
+                            <input  type="text" id="birthday" name="birthday" style="width:170px;padding:7px 10px;border:1px solid #ccc;margin-right:10px;" placeholder="出生日期"></div>
                         </td>
                     </tr>
                     <tr>
@@ -124,17 +125,7 @@
     <script>
         $(function () {
             $(".add_tab input").attr('class','lg');//给输入框添加样式
-            $("#birthday").calendar({
-                controlId: "divDate",                                 // 弹出的日期控件ID，默认: $(this).attr("id") + "Calendar"
-                speed: 200,                                           // 三种预定速度之一的字符串("slow", "normal", or "fast")或表示动画时长的毫秒数值(如：1000),默认：200
-                complement: true,                                     // 是否显示日期或年空白处的前后月的补充,默认：true
-                readonly: true,                                       // 目标对象是否设为只读，默认：true
-                upperLimit: new Date(),                               // 日期上限，默认：NaN(不限制)
-                lowerLimit: new Date(),                   // 日期下限，默认：NaN(不限制)
-                callback: function () {                               // 点击选择日期后的回调函数
-                    $("#birthday").val();
-                }
-            });
+            $("#birthday").calendar();
         })
     </script>
 @endsection
