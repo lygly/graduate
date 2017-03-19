6fb16b62d878d359c08e1f98ae0b515c622b88b2
lygly <1516045937@qq.com>
2017-03-19 23:36:00 +0800

    日期插件设置中

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{asset('/resources/views/admin/style/css/ch-ui.admin.css')}}">
     <link rel="stylesheet" href="{{asset('/resources/views/admin/style/font/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/app/org/lyz.calendar/css/lyz.calendar.css')}}">{{--日期插件--}}
     <script type="text/javascript" src="{{asset('/resources/views/admin/style/js/jquery.js')}}"></script>
     <script type="text/javascript" src="{{asset('/resources/views/admin/style/js/ch-ui.admin.js')}}"></script>
     <script type="text/javascript" src="{{asset('/app/org/layer/layer.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/app/org/lyz.calendar/lyz.calendar.min.js')}}"></script>{{--日期插件--}}
 </head>
 <body>
 @yield('content')
