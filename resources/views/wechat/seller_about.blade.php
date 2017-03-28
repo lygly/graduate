eab42386209dbb3c1852b5bc2d5d2ffa19037ace
lygly <1516045937@qq.com>
2017-03-28 18:14:11 +0800

    微信网页授权进行中

--- resources/views/wechat/seller_about.blade.php
+++ resources/views/wechat/seller_about.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.wechat')
 @section('content')
 <div class="x-container">
-    <div class="about">
-        <img src="{{url('/resources/views/wechat/static/img/seller_about.jpeg')}}" alt="">
+    <div class="weui-gallery about" style="display: block;">
+{{--        <img src="{{url('/resources/views/wechat/static/img/seller_about.jpeg')}}" alt="">--}}
+        <span class="weui-gallery__img" style="background-image: url(/resources/views/wechat/static/img/seller_about.jpeg)"></span>
     </div>
 
 </div>
